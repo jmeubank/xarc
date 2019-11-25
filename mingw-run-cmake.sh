06849944739482a7e9da581fe427decfa3c870f4
John E <john@thesnappy.net>
2019-11-25 15:47:59 -0800

    Build with external bzip2

--- mingw-run-cmake.sh
+++ mingw-run-cmake.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 
+export BZIP2_URL="file://C:/Users/joeub/Downloads/bzip2-1.0.8.tar.gz"
+export BZIP2_DIRNAME="bzip2-1.0.8"
 export LZMA_URL="file://C:/Users/joeub/Downloads/lzma1900.7z"
 export LZMA_FILENAME="lzma1900.7z"
 export LZMA_DIRNAME="lzma"
@@ -8,7 +10,14 @@ export ZLIB_DIRNAME="zlib-1.2.11"
 
 mkdir -p extlibs && \
   ( \
-    [ -d "extlibs/lzma" ] || ( \
+    [ -d "extlibs/$BZIP2_DIRNAME" ] || ( \
+      mkdir -p extlibs && \
+      cd extlibs && \
+      curl -#L -o- "$BZIP2_URL" | bsdtar -xf - \
+    ) \
+  ) && \
+  ( \
+    [ -d "extlibs/lzma/C" ] || ( \
       mkdir -p extlibs/lzma && \
       cd extlibs/lzma && \
       curl -#JL -O "$LZMA_URL" && \
