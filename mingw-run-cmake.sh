475e2db2e79232169e819535e1043e3bf1c53f58
John E <john@thesnappy.net>
2019-11-25 15:39:12 -0800

    Build zlib from external sources

--- mingw-run-cmake.sh
+++ mingw-run-cmake.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/sh
 
+export LZMA_URL="file://C:/Users/joeub/Downloads/lzma1900.7z"
+export LZMA_FILENAME="lzma1900.7z"
+export LZMA_DIRNAME="lzma"
+export ZLIB_URL="file://C:/Users/joeub/Downloads/zlib-1.2.11.tar.xz"
+export ZLIB_DIRNAME="zlib-1.2.11"
+
 mkdir -p extlibs && \
-  (
-    [ -d extlibs/lzma ] || ( \
+  ( \
+    [ -d "extlibs/lzma" ] || ( \
       mkdir -p extlibs/lzma && \
       cd extlibs/lzma && \
-      curl -s -O -J -L file://C:/Users/joeub/Downloads/lzma1900.7z && \
-      7z x -y lzma1900.7z \
+      curl -#JL -O "$LZMA_URL" && \
+      7z x $LZMA_FILENAME \
+    ) \
+  ) && \
+  ( \
+    [ -d "extlibs/$ZLIB_DIRNAME" ] || ( \
+      mkdir -p extlibs && \
+      cd extlibs && \
+      curl -#L -o- "$ZLIB_URL" | bsdtar -xf - \
     ) \
   ) && \
   cmake -G "MSYS Makefiles" $(dirname "$0") && \
