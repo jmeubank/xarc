2b74dd9c15f1aabe157fad3c744f06be18fc9226
John E <john@thesnappy.net>
2019-11-30 10:16:02 -0800

    Fix quotes on echo commands, only run cmake explicitly if Makefile doeson't exist, and use verbose output when running mingw32-make

--- mingw-build.cmd
+++ mingw-build.cmd
@@ -18,14 +18,14 @@ set "ZLIB_DIRNAME=zlib-1.2.11"
 
 if not exist "%WD%extlibs/" ( mkdir "%WD%extlibs" || exit /b %ERRORLEVEL% )
 if not exist "%WD%extlibs/%BZIP2_DIRNAME%/" (
-    echo "Downloading and extracting BZIP2"
+    echo Downloading and extracting BZIP2
     pushd "%WD%/extlibs"
     curl -#JLO "%BZIP2_URL%" || exit /b %ERRORLEVEL%
     ( "%SZ_EXE%" x -so -y "%BZIP2_FILENAME%" | "%SZ_EXE%" x -si -y -ttar ) || exit /b %ERRORLEVEL%
     popd
 )
 if not exist "%WD%extlibs/%LZMA_DIRNAME%/C" (
-    echo "Downloading and extracting LZMA SDK"
+    echo Downloading and extracting LZMA SDK
     if not exist "%WD%extlibs/%LZMA_DIRNAME%/" ( mkdir "%WD%extlibs/%LZMA_DIRNAME%" || exit /b %ERRORLEVEL% )
     pushd "%WD%extlibs/%LZMA_DIRNAME%"
     curl -#JLO "%LZMA_URL%" || exit /b %ERRORLEVEL%
@@ -33,13 +33,17 @@ if not exist "%WD%extlibs/%LZMA_DIRNAME%/C" (
     popd
 )
 if not exist "%WD%extlibs/%ZLIB_DIRNAME%" (
-    echo "Downloading and extracting ZLIB"
+    echo Downloading and extracting ZLIB
     pushd "%WD%extlibs"
     curl -#JLO "%ZLIB_URL%" || exit /b %ERRORLEVEL%
     ( "%SZ_EXE%" x -so -y "%ZLIB_FILENAME%" | "%SZ_EXE%" x -si -y -ttar ) || exit /b %ERRORLEVEL%
     popd
 )
-cmake.exe -G "MinGW Makefiles" "%~dp0" || exit /b %ERRORLEVEL%
-echo "going multithreaded"
+
+if not exist "%WD%Makefile" ( cmake.exe -G "MinGW Makefiles" "%~dp0" || exit /b %ERRORLEVEL% )
+
+echo Going multithreaded
+set VERBOSE=1
 mingw32-make.exe -j12  || exit /b %ERRORLEVEL%
+
 exit /b 0
