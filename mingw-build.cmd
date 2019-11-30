b071d82b1b0be9c6aff2da0534040ce2d7986900
John E <john@thesnappy.net>
2019-11-30 11:00:24 -0800

    Use 7z.exe from PATH, and add CMAKE as a downloadable extlib

--- mingw-build.cmd
+++ mingw-build.cmd
@@ -3,9 +3,13 @@ setlocal
 
 set "WD=%__CD__%"
 
-set "PATH=C:\TDM-GCC-32\bin;%PATH%"
+REM the PATH environment needs to include 7z.exe, mingw32-make.exe and a working MinGW or MinGW-w64 GCC
+REM installation.
+set "PATH=C:\Program Files\7-Zip;C:\TDM-GCC-32\bin;%PATH%"
 
-set "SZ_EXE=C:/Program Files/7-Zip/7z.exe"
+set "CMAKE_URL=file://C:/Users/joeub/Downloads/cmake-3.16.0-win32-x86.zip"
+set "CMAKE_FILENAME=cmake-3.16.0-win32-x86.zip"
+set "CMAKE_DIRNAME=cmake-3.16.0-win32-x86"
 set "BZIP2_URL=file://C:/Users/joeub/Downloads/bzip2-1.0.8.tar.gz"
 set "BZIP2_FILENAME=bzip2-1.0.8.tar.gz"
 set "BZIP2_DIRNAME=bzip2-1.0.8"
@@ -16,12 +20,21 @@ set "ZLIB_URL=file://C:/Users/joeub/Downloads/zlib-1.2.11.tar.xz"
 set "ZLIB_FILENAME=zlib-1.2.11.tar.xz"
 set "ZLIB_DIRNAME=zlib-1.2.11"
 
+set "PATH=%WD%extlibs\%CMAKE_DIRNAME%\bin;%PATH%"
+
 if not exist "%WD%extlibs/" ( mkdir "%WD%extlibs" || exit /b %ERRORLEVEL% )
+if not exist "%WD%extlibs/%CMAKE_DIRNAME%/" (
+    echo Downloading and extracting CMAKE
+    pushd "%WD%/extlibs"
+    curl -#JLO "%CMAKE_URL%" || exit /b %ERRORLEVEL%
+    7z.exe x -y "%CMAKE_FILENAME%" || exit /b %ERRORLEVEL%
+    popd
+)
 if not exist "%WD%extlibs/%BZIP2_DIRNAME%/" (
     echo Downloading and extracting BZIP2
     pushd "%WD%/extlibs"
     curl -#JLO "%BZIP2_URL%" || exit /b %ERRORLEVEL%
-    ( "%SZ_EXE%" x -so -y "%BZIP2_FILENAME%" | "%SZ_EXE%" x -si -y -ttar ) || exit /b %ERRORLEVEL%
+    ( 7z.exe x -so -y "%BZIP2_FILENAME%" | 7z.exe x -si -y -ttar ) || exit /b %ERRORLEVEL%
     popd
 )
 if not exist "%WD%extlibs/%LZMA_DIRNAME%/C" (
@@ -29,14 +42,14 @@ if not exist "%WD%extlibs/%LZMA_DIRNAME%/C" (
     if not exist "%WD%extlibs/%LZMA_DIRNAME%/" ( mkdir "%WD%extlibs/%LZMA_DIRNAME%" || exit /b %ERRORLEVEL% )
     pushd "%WD%extlibs/%LZMA_DIRNAME%"
     curl -#JLO "%LZMA_URL%" || exit /b %ERRORLEVEL%
-    "%SZ_EXE%" x -y "%LZMA_FILENAME%" || exit /b %ERRORLEVEL%
+    7z.exe x -y "%LZMA_FILENAME%" || exit /b %ERRORLEVEL%
     popd
 )
 if not exist "%WD%extlibs/%ZLIB_DIRNAME%" (
     echo Downloading and extracting ZLIB
     pushd "%WD%extlibs"
     curl -#JLO "%ZLIB_URL%" || exit /b %ERRORLEVEL%
-    ( "%SZ_EXE%" x -so -y "%ZLIB_FILENAME%" | "%SZ_EXE%" x -si -y -ttar ) || exit /b %ERRORLEVEL%
+    ( 7z.exe x -so -y "%ZLIB_FILENAME%" | 7z.exe x -si -y -ttar ) || exit /b %ERRORLEVEL%
     popd
 )
 
