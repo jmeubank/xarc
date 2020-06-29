f36f3c9ae4177da7c9874c72d8d58e4c019c648f
mimi89999 <michel@lebihan.pl>
2020-06-29 13:00:34 +0200

    Change mingw-build to use download URLs

--- mingw-build.cmd
+++ mingw-build.cmd
@@ -7,17 +7,17 @@ REM the PATH environment needs to include 7z.exe, mingw32-make.exe and a working
 REM installation.
 set "PATH=C:\Program Files\7-Zip;C:\TDM-GCC-32\bin;%PATH%"
 
-set "CMAKE_URL=file://C:/Users/joeub/Downloads/cmake-3.16.0-win32-x86.zip"
-set "CMAKE_FILENAME=cmake-3.16.0-win32-x86.zip"
-set "CMAKE_DIRNAME=cmake-3.16.0-win32-x86"
-set "BZIP2_URL=file://C:/Users/joeub/Downloads/bzip2-1.0.8.tar.gz"
+set "CMAKE_URL=https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3-win32-x86.zip"
+set "CMAKE_FILENAME=cmake-3.17.3-win32-x86.zip"
+set "CMAKE_DIRNAME=cmake-3.17.3-win32-x86"
+set "BZIP2_URL=https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz"
 set "BZIP2_FILENAME=bzip2-1.0.8.tar.gz"
 set "BZIP2_DIRNAME=bzip2-1.0.8"
-set "LZMA_URL=file://C:/Users/joeub/Downloads/lzma1900.7z"
+set "LZMA_URL=https://sourceforge.net/projects/sevenzip/files/LZMA%%20SDK/lzma1900.7z/download"
 set "LZMA_FILENAME=lzma1900.7z"
 set "LZMA_DIRNAME=lzma"
-set "ZLIB_URL=file://C:/Users/joeub/Downloads/zlib-1.2.11.tar.xz"
-set "ZLIB_FILENAME=zlib-1.2.11.tar.xz"
+set "ZLIB_URL=https://github.com/madler/zlib/archive/v1.2.11.tar.gz"
+set "ZLIB_FILENAME=zlib-1.2.11.tar.gz"
 set "ZLIB_DIRNAME=zlib-1.2.11"
 
 set "PATH=%WD%extlibs\%CMAKE_DIRNAME%\bin;%PATH%"
@@ -26,14 +26,14 @@ if not exist "%WD%extlibs/" ( mkdir "%WD%extlibs" || exit /b %ERRORLEVEL% )
 if not exist "%WD%extlibs/%CMAKE_DIRNAME%/" (
     echo Downloading and extracting CMAKE
     pushd "%WD%/extlibs"
-    curl -#JLO "%CMAKE_URL%" || exit /b %ERRORLEVEL%
+    curl -#JL "%CMAKE_URL%" -o "%CMAKE_FILENAME%" || exit /b %ERRORLEVEL%
     7z.exe x -y "%CMAKE_FILENAME%" || exit /b %ERRORLEVEL%
     popd
 )
 if not exist "%WD%extlibs/%BZIP2_DIRNAME%/" (
     echo Downloading and extracting BZIP2
     pushd "%WD%/extlibs"
-    curl -#JLO "%BZIP2_URL%" || exit /b %ERRORLEVEL%
+    curl -#JL "%BZIP2_URL%" -o "%BZIP2_FILENAME%" || exit /b %ERRORLEVEL%
     ( 7z.exe x -so -y "%BZIP2_FILENAME%" | 7z.exe x -si -y -ttar ) || exit /b %ERRORLEVEL%
     popd
 )
@@ -41,14 +41,14 @@ if not exist "%WD%extlibs/%LZMA_DIRNAME%/C" (
     echo Downloading and extracting LZMA SDK
     if not exist "%WD%extlibs/%LZMA_DIRNAME%/" ( mkdir "%WD%extlibs/%LZMA_DIRNAME%" || exit /b %ERRORLEVEL% )
     pushd "%WD%extlibs/%LZMA_DIRNAME%"
-    curl -#JLO "%LZMA_URL%" || exit /b %ERRORLEVEL%
+    curl -#JL "%LZMA_URL%" -o "%LZMA_FILENAME%" || exit /b %ERRORLEVEL%
     7z.exe x -y "%LZMA_FILENAME%" || exit /b %ERRORLEVEL%
     popd
 )
 if not exist "%WD%extlibs/%ZLIB_DIRNAME%" (
     echo Downloading and extracting ZLIB
     pushd "%WD%extlibs"
-    curl -#JLO "%ZLIB_URL%" || exit /b %ERRORLEVEL%
+    curl -#JL "%ZLIB_URL%" -o "%ZLIB_FILENAME%" || exit /b %ERRORLEVEL%
     ( 7z.exe x -so -y "%ZLIB_FILENAME%" | 7z.exe x -si -y -ttar ) || exit /b %ERRORLEVEL%
     popd
 )
